f459140c6f3dbf7f5c55c26b20b2c489b047a2e4
KazeFX <peter.miscevic@gmail.com>
2025-01-02 11:29:03 +0100

    Test new pc

--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -97,7 +97,7 @@ namespace PeopleApp
             var thing1 = ("Neville", 4);
             WriteLine($"{thing1.Item1} has {thing1.Item2} children.");
 
-
+            var thing2 = 
             
 
         }
