e957309148398b86796e816c1f6b2edf249cccff
KazeFX <kaze.fx.1337@gmail.com>
2025-02-02 09:30:17 +0100

    +

--- Chapter07/AssembliesAndNamespaces/Program.cs
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace AssembliesAndNamespaces
+﻿
+using System; // String
+using System.Xml.Linq; // adding namespace for XDocument with 'using'
+using static System.Console;
+
+namespace AssembliesAndNamespaces
 {
-    internal class Program
+    public class Program
     {
+
+        XDocument doc = new();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string s1 = "Hello";
+            string s2 = "World";
+
+            WriteLine($"{s1} {s2}");
         }
     }
 }
