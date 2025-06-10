53f9bcada1c7997c5c8c8be80c8b1347e5eed5b9
KazeFX <peter.miscevic@gmail.com>
2025-06-10 16:11:13 +0200

    +

--- Chapter02/Numbers/Program.cs
+++ Chapter02/Numbers/Program.cs
@@ -74,4 +74,8 @@ Console.WriteLine(line);
 
 #endregion
 
-
+unsafe
+{
+    Console.WriteLine($"Half uses {sizeof(Half)} bytes and can store numbers in the range {Half.MinValue:N0} to {Half.MaxValue:N0}.");
+    Console.WriteLine($"Int128 uses {sizeof(Int128)} bytes and can store numbers int he range {Int128.MinValue:N0} to {Int128.MaxValue:N0}.");
+}
