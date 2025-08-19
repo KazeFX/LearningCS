848b7bc17b8abc0b4a6a5c0c974eb614812edef6
KazeFX <kaze.fx.1337@gmail.com>
2025-08-19 03:43:52 +0200

    +

--- Chapter02/Formatting/Program.cs
+++ Chapter02/Formatting/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System.Globalization; // To use CultureInfo
 
+
 // Set current culture to US English so that all readers see the same output as shown in the book.
 CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
 
 int numberOfApples = 12;
 decimal pricePerApple = 0.35M;
 
-Console.WriteLine(
+WriteLine(
     format: "{0} apples cost {1:C}",
     arg0: numberOfApples,
     arg1: pricePerApple * numberOfApples);
@@ -19,7 +20,7 @@ string formatted = string.Format(
 //WriteToFile(formatted); // Writes the string into a file.
 
 // The following statement must be all on one line when using C# 10 or earlier. If using C# 11 or later, we can include a line break in the middle of an expression but not in the string text.
-Console.WriteLine($"{numberOfApples} apples cost {pricePerApple
+WriteLine($"{numberOfApples} apples cost {pricePerApple
     * numberOfApples:C}");
 
 string applesText = "Apples";
@@ -27,25 +28,30 @@ int applesCount = 1234;
 string bananasText = "Bananas";
 int bananasCount = 56789;
 
-Console.WriteLine();
+WriteLine();
 
-Console.WriteLine(format: "{0,-10} {1,6}",
+WriteLine(format: "{0,-10} {1,6}",
     arg0: "Name", arg1: "Count");
 
-Console.WriteLine(format: "{0,-10} {1,6:N0}",
+WriteLine(format: "{0,-10} {1,6:N0}",
     arg0: applesText, arg1: applesCount);
 
-Console.WriteLine(format: "{0,-10} {1,6:N0}",
+WriteLine(format: "{0,-10} {1,6:N0}",
     arg0: bananasText, arg1: bananasCount);
 
 decimal value = 0.325M;
-Console.WriteLine("Currency: {0:C}, Percentage: {0:0.0%}", value);
+WriteLine("Currency: {0:C}, Percentage: {0:0.0%}", value);
 
-Console.WriteLine("Type your first name and press ENTER: ");
-string firstName = Console.ReadLine();
+WriteLine("Type your first name and press ENTER: ");
+string? firstName = ReadLine();
 
-Console.WriteLine("Type your age and press ENTER: ");
-string age = Console.ReadLine();
+WriteLine("Type your age and press ENTER: ");
+string age = ReadLine()!;
 
-Console.WriteLine($"Hello {firstName}, you look good for {age}.");
+WriteLine($"Hello {firstName}, you look good for {age}.");
 
+Write("Press any key combination: ");
+ConsoleKeyInfo key = ReadKey();
+WriteLine();
+WriteLine("Key: {0}, Char: {1}, Modifier: {2}",
+    arg0: key.Key, arg1: key.KeyChar, arg2: key.Modifiers);
