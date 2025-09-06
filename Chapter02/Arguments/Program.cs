60cefee67b40e32496b6f17285dab9e1bad8820c
KazeFX <kaze.fx.1337@gmail.com>
2025-09-06 19:11:56 +0200

    Finished Ch2 and added Ch3.

--- Chapter02/Arguments/Program.cs
+++ Chapter02/Arguments/Program.cs
@@ -5,9 +5,26 @@ foreach (string arg in args)
     WriteLine(arg);
 }
 
-if (args.Length< 3)
+if (args.Length < 3)
 {
-
+    WriteLine("Specify two colors and cursor size");
+    WriteLine("dotnet run red yellow 50");
+    return; // Stop running.
 }
 
+ForegroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[0], ignoreCase: true);
 
+BackgroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[1], ignoreCase: true);
+
+try
+{
+    CursorSize = int.Parse(args[2]);
+}
+catch (PlatformNotSupportedException)
+{
+    WriteLine("Current platform doesn't support cursor change. >:(");
+}
