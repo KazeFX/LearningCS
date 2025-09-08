872cee95a215133687b31a64c17d9450c76ff4af
KazeFX <kaze.fx.1337@gmail.com>
2025-09-08 22:01:38 +0200

    Post stream

---
+++ Chapter03/CastingConverting/Program.cs
@@ -0,0 +1,57 @@
+﻿using static System.Convert; // To use the ToInt32 method.
+
+int a = 10;
+double b = a; // An int can be safely cast into a double.
+WriteLine($"a is {a}, b is {b}");
+
+double c = 9.8;
+int d = (int)c;
+WriteLine($"c is {c}, d is {d}.");
+
+long e = 10;
+int f = (int)e;
+WriteLine($"e is {e:N0}, f is {f:N0}.");
+
+e = 5_000_000_000;
+f = (int)e;
+WriteLine($"e is {e:N0}, f is {f:N0}."); // 5bil cant fit into a 32bit integer => overflow.
+
+WriteLine("{0,12} {1,34}", "Decimal", "Binary");
+WriteLine("{0,12} {0,34:B32}", int.MaxValue);
+for (int i = 8; i >= -8; i--)
+{
+    WriteLine("{0,12} {0,34:B32}", i);
+}
+WriteLine("{0,12} {0,34:B32}", int.MinValue);
+
+long r = 0b_101000101010001100100111010100101010;
+int s = (int)r;
+WriteLine($"{r,38:B38} = {r}");
+WriteLine($"{s,38:B32} = {s}");
+
+double g = 9.8;
+int h = ToInt32(g); // A method of System.Convert
+WriteLine($"g is {g}, h is {h}");
+
+double[,] doubles =
+{
+    {9.49, 9.5, 9.51 },
+    {10.49, 10.5, 10.51 },
+    {11.49, 11.5, 11.51 },
+    {12.49, 12.5, 12.51 },
+    {-12.49, -12.5, -12.51 },
+    {-11.49, -11.5, -11.51 },
+    {-10.49, -10.5, -10.51 },
+    {-9.49, -9.5, -9.51 },
+};
+
+WriteLine($"| double | ToInt32 | double | ToInt32 | double | ToInt32 |");
+for (int x = 0; x < 8; x++)
+{
+    for (int y = 0; y < 3; y++)
+    {
+        WriteLine($"| {doubles[x, y],6} | {ToInt16(doubles[x, y]),7}");
+    }
+    WriteLine("|");
+}
+WriteLine();
--- Chapter03/IterationStatements/Program.cs
+++ Chapter03/IterationStatements/Program.cs
@@ -1,6 +1,45 @@
-﻿int x = 0;
-while (x < 10)
+﻿//int x = 0;
+//while (x < 10)
+//{
+//    WriteLine(x);
+//    x++;
+//}
+
+//string? actualPassword = "Pa$$w0rd";
+//string? password;
+
+//do
+//{
+//    attempts--;
+//    Write("Enter your password: ");
+//    password = ReadLine();
+//}
+//while (password != actualPassword);
+
+//WriteLine("Correcto!");
+
+
+for (int y = 1; y <= 10; y++)
 {
-    WriteLine(x);
-    x++;
+    WriteLine(y);
+}
+
+for (int y = 0; y <= 10; y += 3)
+{
+    WriteLine(y);
+}
+
+string[] names = { "Benny", "Astrid", "Feena" };
+
+foreach (string name in names)
+{
+    WriteLine($"{name} has {name.Length} characters.");
+}
+
+// Foreach indepth:
+System.Collections.IEnumerator e = names.GetEnumerator();
+while (e.MoveNext())
+{
+    string name = (string)e.Current; // Current is ready-only :D
+    WriteLine($"{name} has {name.Length} characters.");
 }
