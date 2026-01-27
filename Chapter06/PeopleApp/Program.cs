b91b6a03bb0990c20447f6e03cfe6575cb0c2f58
KazeFX <kaze.fx.1337@gmail.com>
2026-01-28 00:28:14 +0100

    add

--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -1,54 +1,63 @@
 ﻿using KazeLibrary;
 
-Person fahim = new()
+internal class Program
 {
-    Name = "Fahim",
-    Born = new(year: 2005, month: 3, day: 25, hour: 0, minute: 0, second: 0, offset: TimeSpan.Zero)
-};
+    private static void Main(string[] args)
+    {
+        Person fahim = new()
+        {
+            Name = "Fahim",
+            Born = new(year: 2005, month: 3, day: 25, hour: 0, minute: 0, second: 0, offset: TimeSpan.Zero)
+        };
 
-fahim.WriteToConsole();
+        fahim.WriteToConsole();
 
-// Implementing functionality using methods.
-Person lamech = new() { Name = "Lamech" };
-Person adah = new() { Name = "Adah" };
-Person zillah = new() { Name = "Zillah" };
+        // Implementing functionality using methods.
+        Person lamech = new() { Name = "Lamech" };
+        Person adah = new() { Name = "Adah" };
+        Person zillah = new() { Name = "Zillah" };
 
-// Call the instance method to marry Isa and Jackbauer
-lamech.Marry(adah);
+        // Call the instance method to marry Isa and Jackbauer
+        lamech.Marry(adah);
 
-// Call the static method to marry Jackbauer and Picard.
-Person.Marry(lamech, zillah);
+        // Call the static method to marry Jackbauer and Picard.
+        Person.Marry(lamech, zillah);
 
-lamech.OutputSpouses();
-adah.OutputSpouses();
-zillah.OutputSpouses();
+        lamech.OutputSpouses();
+        adah.OutputSpouses();
+        zillah.OutputSpouses();
 
-// Call the instance method to make a baby.
-Person baby1 = lamech.ProCreateWith(adah);
-baby1.Name = "Jabal";
-WriteLine($"{baby1.Name} was born on {baby1.Born}");
+        // Call the instance method to make a baby.
+        Person baby1 = lamech.ProCreateWith(adah);
+        baby1.Name = "Jabal";
+        WriteLine($"{baby1.Name} was born on {baby1.Born}");
 
-// Call the static method to make a baby
-Person baby2 = Person.Procreate(zillah, lamech);
-baby2.Name = "Tubalcain";
+        // Call the static method to make a baby
+        Person baby2 = Person.Procreate(zillah, lamech);
+        baby2.Name = "Tubalcain";
 
-// Use the * operator to "multiply".
-Person baby3 = lamech * adah;////////////////////////////////////
+        // Use the * operator to "multiply".
+        Person baby3 = lamech * adah;
+        baby3.Name = "Jubal";
 
-adah.WriteChildrenToConsole();
-zillah.WriteChildrenToConsole();
-lamech.WriteChildrenToConsole();
+        Person baby4 = zillah * lamech;
+        baby4.Name = "Naamah";
 
-for (int i = 0; i < lamech.Children.Count; i++)
-{
-    WriteLine(format: "     {0}'s child #{1} is named \"{2}\".",
-        arg0: lamech.Name, arg1: i, arg2: lamech.Children[i].Name);
+        adah.WriteChildrenToConsole();
+        zillah.WriteChildrenToConsole();
+        lamech.WriteChildrenToConsole();
+
+        for (int i = 0; i < lamech.Children.Count; i++)
+        {
+            WriteLine(format: "     {0}'s child #{1} is named \"{2}\".",
+                arg0: lamech.Name, arg1: i, arg2: lamech.Children[i].Name);
+        }
+
+        // Person.Marry(lamech, zillah)
+
+        if (lamech + zillah)
+        {
+            WriteLine($"{lamech.Name} and {zillah.Name} successfully got married.");
+        }
+    }
 }
-
-// Person.Marry(lamech, zillah)
-
-if (lamech + zillah)
-{
-    WriteLine($"{lamech.Name} and {zillah.Name} successfully got married.");
-}
-
