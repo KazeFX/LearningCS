4259a04fb9d5e0c76e7ee91edba89862602fc484
KazeFX <kaze.fx.1337@gmail.com>
2026-02-07 15:54:51 +0100

    +

---
+++ Chapter08/WorkingWithNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics; // To use BigInteger
+
+const int width = 40;
+
+WriteLine("ulong.MaxValue vs a 30-digit BigInteger");
+WriteLine(new string('-', width));
+
+ulong big = ulong.MaxValue;
+WriteLine($"{big,width:N0}");
+
+BigInteger bigger = BigInteger.Parse("123456789012345678901234567890");
+WriteLine($"{bigger,width:N0}");
+
+WriteLine("Multiplying big integers");
+int number1 = 2_000_000_000;
+int number2 = 2;
+WriteLine($"number1: {number1:N0}");
+WriteLine($"number2: {number2:N0}");
+WriteLine($"number1 * number2: {number1 * number2:N0}");
