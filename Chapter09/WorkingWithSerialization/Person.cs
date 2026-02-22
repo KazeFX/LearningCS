3c353475e6f67c501830aa484dd7f906a21783dc
KazeFX <1071525+KazeFX@users.noreply.github.com>
2026-02-22 23:28:49 +0100

    +

---
+++ Chapter09/WorkingWithSerialization/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace Kaze.Shared;
+
+public class Person
+{
+    // A parameterless constructor is required for XML serialization.
+    public Person() { }
+    public Person(decimal initialSalary)
+    {
+        Salary = initialSalary;
+    }
+
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public HashSet<Person>? Children { get; set; }
+    protected decimal Salary { get; set; }
+}
